50fd5d73edae637af0379b4f01e7239a7aa5874d
Thomas Barrett <thomasbarrett@Thomass-MBP.home>
2018-07-23 13:45:46 -0400

    Added another test cast

---
+++ docs/syntax.md
@@ -0,0 +1,8 @@
+# Tomscript Syntax
+
+## Function Declarations
+```swift
+func foo(a: Integer, b: Integer) -> Integer {
+  return a+b;
+}
+```
--- test/src/Parse/DeclParser_Test.cpp
+++ test/src/Parse/DeclParser_Test.cpp
@@ -51,3 +51,17 @@ TEST(StmtParser, parseCompoundStmt) {
 
   EXPECT_NO_THROW(parse("{\nlet a: Integer = 5\nreturn a\n}"));
 }
+
+TEST(StmtParser, parseReturnStmt) {
+  auto parse = [](std::string text) {
+    SourceManager::currentSource = new SourceCode(std::istringstream(text),"test");
+    Parser parser = Parser{SourceManager::currentSource};
+    return parser.parseReturnStmt();
+  };
+
+  EXPECT_NO_THROW(parse("return 5\n"));
+  EXPECT_ANY_THROW(parse("return 5"));
+  EXPECT_NO_THROW(parse("return\n"));
+  EXPECT_ANY_THROW(parse("return"));
+
+}
